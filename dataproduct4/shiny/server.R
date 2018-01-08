33a3215593c217a6eed02188501b0d58e26b72dd
vitalius <vitaliy@cloudmotion.com>
2018-01-07 22:46:28 -0800

    presidents databaset

--- dataproduct4/shiny/server.R
+++ dataproduct4/shiny/server.R
@@ -2,11 +2,21 @@ library(shiny)
 
 shinyServer(function(input, output) {
    
-  output$mpgPlot <- renderPlot({
+  output$drawPlot <- renderPlot({
     
     # generate plot from input from ui.R
-    data = subset(mtcars, cyl==input$cyl)
-    plot(mpg ~ disp, data = data)
+    pdata = pdata = data.frame(r=as.matrix(presidents), t=time(presidents))
+    ydata = pdata[which(pdata$t>=(input$year) & pdata$t<(input$year+1)),]
+    
+    x = c(1,2,3,4)
+    plot(x, ydata$r, xlab="Quater", ylab="Rating", 
+         main=paste("Year", input$year, sep=" "),
+         xlim=c(1,4), ylim=c(0,100), xaxt="n", col="blue")
+    axis(1, at = seq(1, 4, by = 1), las=2)
+    abline(lm(ydata$r ~ x), col="blue")
+    points(x, c(input$q1,input$q2,input$q3,input$q4), pch=20, col="red")
+    legend(1, 95, legend=c("Dataset", "Current Input"),
+           col=c("blue", "red"), lty=1:1, cex=0.8)
   })
   
 })
--- dataproduct4/shiny/ui.R
+++ dataproduct4/shiny/ui.R
@@ -1,34 +1,41 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Flue Efficiency vs Engine Displacement"),
+  titlePanel("US President Approval Ratings"),
   
-  # Sidebar with a slider input for number of cyl 
+  # Sidebar with inputs
   sidebarLayout(
     sidebarPanel(
-       sliderInput("cyl",
-                   "Number of cyl:",
-                   min = 4,
-                   max = 8,
-                   step = 2,
-                   value = 4)
+
+      tags$div(class="header", checked=NA,
+               tags$p("View quarterly presidential approval ratings from the past (years 1945-1974) 
+                      and compare to custom values. Default custom values are for president Trump 2017")),
+            
+      tags$div(class="header", checked=NA,
+               tags$p("Select dataset year to compare")),
+      
+       sliderInput("year",
+                   "Year:",
+                   min = 1945,
+                   max = 1974,
+                   step = 1,
+                   value = 1974,
+                   sep = ""),
+      
+      tags$div(class="header", checked=NA,
+               tags$p("Manual input")),
+      
+       numericInput("q1", "Qrt 1", 41.3, min = 1, max = 100),
+       numericInput("q2", "Qrt 2", 38.8, min = 1, max = 100),
+       numericInput("q3", "Qrt 3", 36.9, min = 1, max = 100),
+       numericInput("q4", "Qrt 4", 32.0, min = 1, max = 100)
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("mpgPlot")
+       plotOutput("drawPlot")
     )
   )
 ))
